647efae50ed6237753278a384d1f616fbe2742af
jorgeasmendes <jserpamendes@gmail.com>
2025-10-27 17:56:44 -0300

    feat: criar iteração para baixar e registrar os dados da api de tempos em tempos

--- pipeline/tasks.py
+++ pipeline/tasks.py
@@ -1,20 +1,32 @@
 import requests
 import pandas as pd
+from datetime import datetime
+import pytz
+import time
+import os
+
 
 #Funções para baixar os dados da API e salvar em csv
-def download_data():
+def download_data(i):
     try:
         response = requests.get("https://dados.mobilidade.rio/gps/brt")
         response.raise_for_status()
     except response.status_code() as e:
-        print(f"Erro ao fazer download, código: {e}")
+        print(f"Erro ao fazer download de número {i}, código: {e}")
         return None
     else:
-        print("Download realizado com sucesso")
+        print(f"Download da iteração {i} realizado com sucesso")
         return response.json()
 
-def to_csv(json_data):
-    df = pd.DataFrame(json_data["veiculos"])
-    print(df.head())
-
-to_csv(download_data())
+def api_to_csv():
+    for i in range(1,5):
+        print(f"Começando iteração {i}...")
+        dict_data = download_data(i)
+        df = pd.DataFrame(dict_data["veiculos"])
+        df["datetime_registro"] = datetime.now(pytz.timezone('America/Sao_Paulo'))
+        file_exists = os.path.exists("brt-dados.csv")
+        df.to_csv("brt-dados.csv", mode='a', index=False, header=not file_exists)
+        print(f"Iteração {i} concluída")
+        time.sleep(10)
+        
+api_to_csv()
