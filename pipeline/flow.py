e849a08eaadeb3b4ad458c4c36690f02333e3c9e
jorgeasmendes <jserpamendes@gmail.com>
2025-10-27 22:01:18 -0300

    fix: definir storage do flow no github e corrigir estrutura de import para execução correta de docker agent

--- pipeline/flow.py
+++ pipeline/flow.py
@@ -1,5 +1,8 @@
-from pipeline.tasks import api_to_csv
+from tasks import api_to_csv
 from prefect import Flow
+from prefect.storage import GitHub
 
 with Flow("brt-pipeline") as flow:
     api_to_csv()
+
+flow.storage = GitHub(repo="jorgeasmendes/prefeitura-brt-pipeline",path="pipeline/flow.py")
