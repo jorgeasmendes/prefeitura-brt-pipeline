c85494bbadb6e1e26ce7ea690177affba26c84b3
jorgeasmendes <jserpamendes@gmail.com>
2025-10-28 15:46:44 -0300

    feat: configurar toda a execução e documentação do dbt

---
+++ pipeline/dbt_project/models/brt_gold.sql
@@ -0,0 +1,33 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key="codigo_datahora",
+        partition_by={
+        "field": "DATE(datahora_captura_registro)",
+        "data_type": "date"
+        },
+        cluster_by=["linha","codigo"],
+        merge_update_partition_filter="DATE(datahora_captura_registro) >= DATE_SUB(CURRENT_DATE(), INTERVAL {{ var('dias_historico_brt_silver') }} DAY)"
+    )
+}}
+
+SELECT 
+    codigo,
+    placa,
+    linha,
+    latitude,
+    longitude,
+    datahora_emissao_registro,
+    velocidade,
+    id_migracao_trajeto,
+    sentido,
+    trajeto,
+    hodometro,
+    direcao,
+    ignicao,
+    datahora_captura_registro,
+    CONCAT(codigo, '_', CAST(datahora_emissao_registro AS STRING)) AS codigo_datahora
+FROM {{ ref('brt_silver') }}
+{% if is_incremental() %}
+WHERE datahora_captura_registro > (SELECT MAX(datahora_captura_registro) FROM {{ this }})
+{% endif %} 
---
+++ pipeline/dbt_project/models/brt_silver.sql
@@ -0,0 +1,30 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
+SELECT 
+    codigo,
+    placa,
+    linha,
+    SAFE_CAST(latitude AS FLOAT64) AS latitude,
+    SAFE_CAST(longitude AS FLOAT64) AS longitude,
+    DATETIME(SAFE.TIMESTAMP_MILLIS(SAFE_CAST(dataHora AS INT64)), 'America/Sao_Paulo') AS datahora_emissao_registro,
+    SAFE_CAST(velocidade AS FLOAT64) AS velocidade,
+    id_migracao_trajeto,
+    sentido,
+    trajeto,
+    SAFE_CAST(hodometro AS FLOAT64) AS hodometro,
+    direcao,
+    ignicao='1' AS ignicao,
+    SAFE_CAST(capacidadePeVeiculo AS INT64) AS capacidade_passageiros_pe,
+    SAFE_CAST(capacidadeSentadoVeiculo AS INT64) AS capacidade_passageiros_sentados,
+    DATETIME(SAFE.PARSE_TIMESTAMP('%Y-%m-%d %H:%M:%E6S%Ez', datetime_registro), 'America/Sao_Paulo') AS datahora_captura_registro,
+    date AS data_captura_registro
+FROM {{ source('brt_dataset', 'brt_bronze')}} WHERE date >= DATE_SUB(CURRENT_DATE(), INTERVAL {{ var('dias_historico_brt_silver')}} DAY)
+QUALIFY ROW_NUMBER() OVER (
+    PARTITION BY codigo, datahora_emissao_registro
+    ORDER BY datahora_captura_registro DESC) = 1
+
+
